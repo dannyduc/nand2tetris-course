1a36fe31552aeb42daddabb8741b2af7ba3a88a0
Danny Nguyen <danny.duc@apple.com>
2016-11-06 12:16:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# nad2tetris-course
+
+https://www.coursera.org/learn/build-a-computer/home/welcome
+
+nand2tetris/tools/HardwareSimulator.sh
+
+http://www.nand2tetris.org/
---
+++ notes/submit.sh
@@ -0,0 +1,1 @@
+zip -r project1.zip Not.hdl And.hdl Or.hdl Xor.hdl Mux.hdl DMux.hdl Not16.hdl And16.hdl Or16.hdl Mux16.hdl Or8Way.hdl Mux4Way16.hdl Mux8Way16.hdl DMux4Way.hdl DMux8Way.hdl
