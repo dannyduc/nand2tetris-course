cc16dca3df98ec672aa4e047ef9cf98f146a6ce7
Danny Nguyen <danny.duc@apple.com>
2016-11-06 12:24:21 -0800

    command for creating project0.zip file for submit

---
+++ nand2tetris/projects/00/cmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Project0
+# If you are taking the course as an auditor, just check that you can locate 
+# file.txt in the nand2tetris/projects/00 folder. If you are taking 
+# the certificate option, submit project0.zip, containing the file 
+# nand2tetris/projects/00/file.txt
+#
+
+zip -r project0.zip file.txt
+
+# upload using the couse interface
