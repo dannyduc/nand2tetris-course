a98497002e128559f0b101b4c3f5a88fa4535a7e
Danny Nguyen <danny.duc@apple.com>
2016-11-08 16:02:49 -0800

    project1 gate implementation

--- nand2tetris/projects/01/cmd.sh
+++ nand2tetris/projects/01/cmd.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-#
-# Project0
-# If you are taking the course as an auditor, just check that you can locate 
-# file.txt in the nand2tetris/projects/00 folder. If you are taking 
-# the certificate option, submit project0.zip, containing the file 
-# nand2tetris/projects/00/file.txt
-#
+# Project 1: Elementary Logic Gates
+# implement chips in order
+# http://www.nand2tetris.org/01.php
 
-zip -r project0.zip file.txt
+zip -r project01.zip Not.hdl And.hdl Or.hdl Xor.hdl Mux.hdl DMux.hdl Not16.hdl And16.hdl Or16.hdl Mux16.hdl Or8Way.hdl Mux4Way16.hdl Mux8Way16.hdl DMux4Way.hdl DMux8Way.hdl
 
 # upload using the couse interface
